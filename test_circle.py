bc6736d55a8b915831f6ce2abccf4003e46b60de
israfilovakira <ik9657110000@gmail.com>
2024-12-27 02:51:01 +0300

    change code 5

--- test_circle.py
+++ test_circle.py
@@ -1,21 +1,33 @@
 import unittest
 from circle import circle_area, circle_perimeter
+import math
+
 class CircleTestCase(unittest.TestCase):
-    def test_area_zero_value(self):
-        res = circle_area(0)
-        self.assertEqual(res, 0)
-    def test_area_positive_value(self):
-        res = circle_area(10)
-        self.assertAlmostEqual(res, 314.1592653589793)
-    def test_perimeter_zero_value(self):
-        res = circle_perimeter(0)
-        self.assertEqual(res, 0)
-    def test_perimeter_positive_value(self):
-        res = circle_perimeter(10)
-        self.assertAlmostEqual(res, 62.83185307179586)
-    def test_area_negative_value(self):
+    def test_circle_area(self):
+        """Площадь круга: радиус положительный."""
+        self.assertAlmostEqual(circle_area(5), math.pi * 25)
+    
+    def test_circle_area_zero(self):
+        """Площадь круга: радиус равен 0."""
+        self.assertEqual(circle_area(0), 0)
+
+    def test_circle_area_negative(self):
+        """Площадь круга: радиус отрицательный."""
         with self.assertRaises(ValueError):
-            circle_area(-10)
-    def test_perimeter_negative_value(self):
+            circle_area(-5)
+
+    def test_circle_perimeter(self):
+        """Длина окружности: радиус положительный."""
+        self.assertAlmostEqual(circle_perimeter(5), 2 * math.pi * 5)
+
+    def test_circle_perimeter_zero(self):
+        """Длина окружности: радиус равен 0."""
+        self.assertEqual(circle_perimeter(0), 0)
+
+    def test_circle_perimeter_negative(self):
+        """Длина окружности: радиус отрицательный."""
         with self.assertRaises(ValueError):
-            circle_perimeter(-10)
+            circle_perimeter(-5)
+
+if __name__ == "__main__":
+    unittest.main()
